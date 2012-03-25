afa74638cbdc1d96bd8d12836da1c419c6daffad
ChengHan Lu <chenghan_lu@berkeley.edu>
2012-03-24 21:59:05 -0700

    admin add another admin, admin assigns table spec, by S

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,51 +1,46 @@
-################################
-# NEED TO BE FIXED
-# SHOULD ADD ADMIN, NOT TC!!!!!
-################################
-
 require 'spec_helper'
 
 describe Admin do
-  describe "add a table cpatain" do
+  describe "testing the functionalities to add admins" do
     before :each do  
       admin = Factory(:admin)
-      tc = Factory(:tableCaptain)
+      new_ad = Factory(:admin, :login => "jess", :name => "Jessica", :password => "Ho")
     end
-    it "should add a table captain" do
-      TableCaptain.should_receive(:find_by_login).with(tc.login).and_return(nil)
-      error = admin.add_table_captain(tc.login, tc.name, tc.password)
+    it "should add an admin" do
+      Admin.should_receive(:find_by_login).with(new_ad.login).and_return(nil)
+      error = admin.add_admin(new_ad.login, new_ad.name, new_ad.password)
       
       error.should == false
       
-      new_tc = TableCaptain.find_by_login(tc.login)
+      added_ad = Admin.find_by_login(new_ad.login)
       
       #check whether we add the correct info for the table captain
-      new_tc.login.should == tc.login
-      new_tc.name.should == tc.name
-      new_tc.password.should == tc.password
+      added_ad.login.should == new_ad.login
+      added_ad.name.should == new_ad.name
+      added_ad.password.should == new_ad.password
     end
     
-    it "should not add a table captain if the login already exists" do
-      TableCaptain.should_receive(:find_by_login).with(tc.login).and_return(nil)
-      error = admin.add_table_captain(tc.login, tc.name, tc.password)
+    it "should not add an admin if the login already exists" do
+      Admin.should_receive(:find_by_login).with(new_ad.login).and_return(nil)
+      error = admin.add_admin(new_ad.login, new_ad.name, new_ad.password)
       
       error.should == false
       
-      tc2 = Factory(:tableCaptain, :name => "Adrian", :password => "wei")
+      ad2 = Factory(:Admin, :name => "Brian", :password => "lin")
       
-      TableCaptain.should_receive(:find_by_login).with(tc2.login).and_return(tc)
-      error = admin.add_table_captain(tc2.login, tc2.name, tc2.password)
+      Admin.should_receive(:find_by_login).with(ad2.login).and_return(new_ad)
+      error = admin.add_table_captain(ad2.login, ad2.name, ad2.password)
       
       error.should == true
       
-      old_tc = TableCaptain.find_by_login(tc.login)
+      old_ad = Admin.find_by_login(new_ad.login)
       
       #check whether we add the correct info for the table captain
-      old_tc.login.should == tc.login
-      old_tc.name.should == tc.name
-      old_tc.password.should == tc.password
-      old_tc.name.should not == tc2.name
-      old_tc.password.should not == tc2.password
+      old_ad.login.should == new_ad.login
+      old_ad.name.should == new_ad.name
+      old_ad.password.should == new_ad.password
+      old_ad.name.should not == ad2.name
+      old_ad.password.should not == ad2.password
     end
   end
 end
--- spec/views/index_spec.rb
+++ spec/views/index_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper'
 
 describe "index.html.haml" do
-  it "should show the add table button when the user is an admin" do
+  it "should show the add table captain button when the user is an admin" do
     admin = Factory(:admin)
     
     render
     rendered.should contain("Add Table Captain")
   end
   
-  it "should show the add table button when the user is not an admin" do
+  it "should not show the add table captain button when the user is not an admin" do
     tc = Factory(:tableCaptain)
     
     render
     rendered.should not contain("Add Table Captain")
+  end
+  it "should show the add table button when the user is an admin" do
+    admin = Factory(:admin)
+    
+    render
+    rendered.should contain("Assign Table")
+  end
+  
+  it "should not show the add table button when the user is not an admin" do
+    tc = Factory(:tableCaptain)
+    
+    render
+    rendered.should not contain("Assign Table")
   end
 end
