b21b9024fe126bab349405834c3567631e2a9d67
Bill Zhao <tauneutrino@gmail.com>
2012-03-27 22:15:08 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pacha2::Application.config.secret_token = 'e3690785e75cc200c7025399f78dd07e29577b82717aa55b01e626895b1003a7597d1879ed94690618489eba344978000fda1355d5465198f615c11a47ccda98'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Pacha2::Application.routes.draw do
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => "home#index"
+  devise_for :users
+  resources :users, :only => :show
+end
