534c2089d4af39c6fddfe07a813b8fc1919ee098
Bill Zhao <tauneutrino@gmail.com>
2012-04-07 18:17:50 -0700

    Revert "Bill removed sign_up from devise"
    This reverts commit 2c98a95887a9aac32e2bc893ec55af235ccdfdc0.
    
    This Fixed the edit_user_registration_path not found message.
    it seems that the devise use :registerable in the model create edit_user_registration_path.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :registerable, :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
