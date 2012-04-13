bd83565be682e99fed91df78cea65d412b34d1c2
Bill Zhao <tauneutrino@gmail.com>
2012-04-12 21:38:08 -0700

    created admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
+admin = Role.create! :name => "Admin"
+admin.users << user
