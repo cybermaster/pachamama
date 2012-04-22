d8606fa4cf9577c9f2165bda4deab5f786fecb46
Bill Zhao <tauneutrino@gmail.com>
2012-04-21 18:29:22 -0700

    bill: fixed all the  merge typo

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,10 @@ Pacha2::Application.routes.draw do
     root :to => 'home#index'
   end
   root :to => "home#index"
-<<<<<<< HEAD
   devise_for :admins
-=======
 
   # the path_names use to change the default sign_up to register
   # devise_for :users, :path_names => { :sign_up => "register"}
->>>>>>> stag
   devise_for :users
   resources :roles
   resources :dining_tables
@@ -22,7 +19,6 @@ Pacha2::Application.routes.draw do
     put "/users" => "users#update"
     get "/edit/user" => "users#edit", :as => :edit_user_registeration
     put "/assets" => "dining_tables#set_table_leader"
-
   end
 
 end
