fce4a861d8f5b7b7b53fb1f80a4c1d1afab10827
Bill Zhao <tauneutrino@gmail.com>
2012-04-21 17:07:11 -0700

    bill: don't need the model spec

--- spec/models/dining_table_spec.rb
+++ spec/models/dining_table_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe DiningTable do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#describe DiningTable do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
