ba5fec82cb46b5d047db38bf66807c7ef4132c55
Bill Zhao <tauneutrino@gmail.com>
2012-04-23 16:31:17 -0700

    Adrian: fixed bug with group numbers

--- app/models/dining_table.rb
+++ app/models/dining_table.rb
@@ -6,7 +6,7 @@ class DiningTable < ActiveRecord::Base
   attr_accessible :table_leader, :physical_number, :group_number,:user_id, :event_id, :table_leader_id
   
   validates_presence_of :group_number#, :physical_number I don't think we need this becuase they dont assign until end
-  validates_uniqueness_of :group_number#, :physical_number
+  #validates_uniqueness_of :group_number, :physical_number
   
   validate :validate_num_users
 
