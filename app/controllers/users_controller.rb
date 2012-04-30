72ae7501fe2101092174771b3a1d79734410a678
Bill Zhao <tauneutrino@gmail.com>
2012-04-29 20:13:58 -0700

    Bill: list of users in ASC order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.order('users.id ASC').all
 
     respond_to do |format|
       format.html # index.html.erb
