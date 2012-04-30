27b6bd85ec62f439d9ae54550eeec10bd74173d5
Adrian Stern <adrian.stern@berkeley.edu>
2012-04-29 17:07:35 -0700

    Adrian: site should now only show everts of current user, unless that user is admin then it shows all, steven says he will be changing the code to use a has_many relationship, I dont know how to do that

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,22 @@ class HomeController < ApplicationController
   def index
     @current_user = current_user
 
-    if current_user.role?("Admin")
+    if current_user.role? :Admin
       @events = Event.all
     else
-      @events = Event.all
+      @events = events_for_current_user
       #@event = Event.where("user_id = ?", current_user.id)
     end
 
+  end
 
+  def events_for_current_user
+    dining_tables = DiningTable.where("user_id = ?", current_user.id)
+    events = Set.new
+    dining_tables.each do |dt|
+      events.add(Event.find(dt.event_id))
+    end
+
+    return events
   end
 end
