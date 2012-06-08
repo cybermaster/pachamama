6e14e616dddd37c894e0401bd87169bd7656c56f
Bill Zhao <tauneutrino@gmail.com>
2012-06-07 18:21:00 -0700

    bill: added the first and last name validations

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,6 +2,11 @@ class Guest < ActiveRecord::Base
   belongs_to :dining_table
   attr_accessible :email, :first_name, :last_name, :note, :phone, :dining_table_id
 
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :uniqueness => true, :email_format => true
+
+
   def first_namelast_name
     name = first_name + " " + last_name
     return name
